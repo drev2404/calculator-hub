3bdbe2940b94a9b541bdda0d3300ebdc24885875
drev2404 <rev2404@gmail.com>
2026-05-01 12:58:30 -0400

    Update recipe.js

--- api/recipe.js
+++ api/recipe.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   try {
     const { ingredients } = req.body;
 
@@ -42,4 +42,4 @@ export default async function handler(req, res) {
       details: error.message
     });
   }
-}
+};
